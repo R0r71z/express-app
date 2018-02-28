eed3c3902244d76f728fb595eec4b5d1f27f4d73
Raddelyn Ortiz <rortiz@intellisys.com.do>
2018-02-28 13:11:53 -0400

    Basic Blog Post Writing Functions

--- apphandlers/requesthandlers.js
+++ apphandlers/requesthandlers.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const {IDfy} = require('./helpers');
 
 class MainHandler {
   static get(request, result) {
@@ -36,21 +37,58 @@ class TestHandler {
 class PostHandler {
   static get(request, result) {
     var _id = request.query.id;
-    fs.readFile('./posts.json', function(err, data) {
+    fs.readFile('../posts.json', function(err, data) {
       if (err) throw err;
-      if (!_id) {
-        const context = {
-          posts: JSON.parse(data)
-        }
-        return result.render('postsview.liquid', context);
-      } else {
+      if (_id) {
         const context = {
           post: JSON.parse(data)[_id]
         }
-        return result.render('singlepostview.liquid', context);
+        // If _id is valid or exists
+        if (context.post) {
+          return result.render('singlepostview.liquid', context);
+        }
       }
+      const context = {
+        posts: JSON.parse(data)
+      }
+      return result.render('postsview.liquid', context);
     });
   }
-}
+
+  static post(request, result) {
+    var _id = request.body.id;
+    var _action = request.body.action;
+    fs.readFile('../posts.json', function(err, data) {
+      var src = JSON.parse(data);
+      if (err) throw err;
+
+      switch (_action) {
+        case 'save':
+          var new_title = request.body.title;
+          var new_content = request.body.content;
+          src[_id].title = new_title;
+          src[_id].content = new_content;
+          break;
+        case 'delete':
+          delete src[_id];
+          break;
+        case 'create':
+          _id = IDfy(src);
+          var title = request.body.title;
+          var content = request.body.content;
+          src[_id] = {
+            title: title,
+            content: content,
+            id: _id
+          }
+      }
+      fs.writeFile('../posts.json', JSON.stringify(src), 'utf8', function(err) {
+        if(err) throw err;
+        result.send({id: _id});
+      })
+    })
+    return 0;
+  }
+};
 
 module.exports = {MainHandler, AdminPageHandler, TestHandler, PostHandler};
---
+++ apphandlers/static/js/singlepost.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+  const delete_callback = function () {
+    var post_id = $(this).attr('target');
+    $.post('/posts', {
+      id: post_id,
+      action: 'delete'
+    });
+    $('.container').empty().append('<h2>This post has been deleted</h2>')
+    setTimeout(function () {
+      location.href='/posts';
+    }, 2500);
+  };
+  const edit_callback = function() {
+    var post_id = $(this).attr('target');
+    var title_elem = $('#post_' + post_id + '_title');
+    var content_elem = $('#post_' + post_id + '_content');
+
+    $('.title-editor').attr('type', 'text');
+    $('.content-editor').attr('type', 'text');
+
+    $('.delete-post')
+    .attr('class', 'cancel-action')
+    .text('Cancel')
+    .off('click')
+    .click(function() {
+      $('.title-editor').attr('type', 'hidden');
+      $('.content-editor').attr('type', 'hidden');
+      $('.save-post')
+      .attr('class', 'edit-post')
+      .text('Edit')
+      .on('click')
+      .click(edit_callback);
+
+      $(this)
+      .attr('class', 'delete-post')
+      .text('Delete')
+      .on('click')
+      .click(delete_callback)
+    })
+
+    $(this)
+    .attr('class', 'save-post')
+    .text('Save')
+    .off('click')
+    .click(function() {
+      var new_title = $('.title-editor').val();
+      var new_content = $('.content-editor').val();
+      $.post('/posts', {
+        action: 'save',
+        id: post_id,
+        title: new_title,
+        content: new_content
+      });
+
+      title_elem.text(new_title);
+      content_elem.text(new_content);
+
+      $('.title-editor').attr('type', 'hidden');
+      $('.content-editor').attr('type', 'hidden');
+
+      $(this)
+      .attr('class', 'edit-post')
+      .text('Edit')
+      .on('click')
+      .click(edit_callback);
+
+      $('.cancel-action')
+      .attr('class', 'delete-post')
+      .text('Delete')
+      .on('click')
+      .click(delete_callback);
+    });
+  }
+
+  $('.edit-post').on('click', edit_callback);
+  $('.delete-post').on('click', delete_callback);
+})
